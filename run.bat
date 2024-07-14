9edfc5a2df4761d51b1e834e4870cba7ae55af75
hadley.patchett <hadley.patchett@gmail.com>
2024-07-14 15:22:22 +0100

    update run.bat for windows, and also .gitignore to ensure tfplan doesnt get added

--- run.bat
+++ run.bat
@@ -2,7 +2,4 @@ docker build rates/ -t cfptestregistry007.azurecr.io/cfp_app:latest
 docker build db/ -t cfptestregistry007.azurecr.io/cfp_db:latest
 terraform init
 terraform apply -target=azurerm_container_registry.registry
-az acr login --name cfptestregistry007
-docker push cfptestregistry007.azurecr.io/cfp_app:latest
-docker push cfptestregistry007.azurecr.io/cfp_db:latest
-terraform plan -out=tfplan
+az acr login --name cfptestregistry007 --only-show-errors && docker push cfptestregistry007.azurecr.io/cfp_app:latest && docker push cfptestregistry007.azurecr.io/cfp_db:latest && terraform plan -out=tfplan
